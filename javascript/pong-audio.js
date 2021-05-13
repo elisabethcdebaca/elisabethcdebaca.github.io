f608f0b9015510b363d6b9656b0f6d08ff2b928d
Elisabeth C'deBaca <elisabethcdebaca@my.unt.edu>
2021-05-13 14:43:41 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -46,7 +46,7 @@ export var soundArray = []; //list of sounds loaded
 export var wallSound = new soundFile("tap-resonant.mp3"); //load sound 
 soundArray.push(wallSound); //add sound to list of sounds
 
-export var paddleSound = new soundFile("cougarpong.mp3");
+export var paddleSound = new soundFile("Cougarpong.mp3");
 soundArray.push(paddleSound);
 
 export var scoreSound = new soundFile("phaserDown2.mp3");
