820e56bb69668e2b7257faa195349d1961d02de6
Elisabeth C'deBaca <elisabethcdebaca@my.unt.edu>
2021-05-20 17:36:09 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -55,14 +55,14 @@ soundArray.push(scoreSound);
 export var catrave = new soundFile("catrave.mp3.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = -16; //turn down volume
+ambientSound.player.volume.value = -256; //turn down volume
 
 export var catrave = new soundFile("catrave.mp3.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
 adventureMusic.player.volume.value = -16;
 
-export var villageMusic = new soundFile("Eight_Bit_Village_Loop.mp3");
+export var catrave = new soundFile("catrave.mp3.mp3");
 soundArray.push(villageMusic);
 villageMusic.player.loop = true;
 villageMusic.player.volume.value = -16;
